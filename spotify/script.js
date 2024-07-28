bd7fea9b59b1645ebe7c5a1e831c6766bbab5391
Damilola91 <168585695+Damilola91@users.noreply.github.com>
2024-07-28 18:29:44 +0200

    Spotify

--- spotify/script.js
+++ spotify/script.js
@@ -1,39 +1,37 @@
 const endPointApi = `https://striveschool-api.herokuapp.com/api/deezer/search?q=`;
 const buttonSearch = document.getElementById("button-search");
 const inputSearch = document.getElementById("searchField");
-const containerResult = document.getElementById("searchResults");
+const containerResult = document.getElementById("eminemSection");
 
 const searchSingers = () => {
     const singerName = inputSearch.value;
-    const nameSingerUrl = `${endPointApi}q=${singerName}`
+    const nameSingerUrl = `${endPointApi}${singerName}`
     return fetch(nameSingerUrl)
     .then(data => data.json())
-    .then(response => {
-        console.log(response)
-        cardSingerResult(response)
-    })
-    
+    .then(data => {
+        console.log(data)
+        containerResult.innerHTML = "";
+        for (let i = 0; i < data.data.length; i++) {
+            cardSingerResult(data.data[i])
+        }
+    })    
 }
- searchSingers()
 
-
-const cardSingerResult = (data) => {
-    
-    containerResult.innerHTML = "";
+const cardSingerResult = (dataCard) => {
     const cardWrapper = document.createElement("div")
-    cardWrapper.setAttribute("class", "card")
-
-    const cardTitle = document.createElement("h3")
-    cardTitle.innerText = data.data.title
+    cardWrapper.setAttribute("class", "card d-flex m-2")
+    const cardTitle = document.createElement("h5")
+    cardTitle.innerText = dataCard.title
     cardTitle.setAttribute("class", "card-title")
-
     const cardDescription = document.createElement("p")
-    cardDescription.innerText = data.data.
+    cardDescription.innerText = dataCard.title_version
     cardDescription.setAttribute("class", "card-text")
-
+    const cardBody = document.createElement("div")
+    cardBody.setAttribute("class", "card-body ")
     const cardImg = document.createElement("img")
-    cardImg.src = data.data.md5_image
-
+    cardImg.src = dataCard.album.cover_medium
+    cardImg.setAttribute("class","card-img-top w-100")
+    cardBody.append(cardTitle, cardDescription)
     containerResult.append(cardWrapper)
-    cardWrapper.append(cardTitle, cardImg, cardDescription)
+    cardWrapper.append(cardImg, cardBody)
 }
